9f3e2a430386d51b81c7d9b504dd58fc7eeae6c0
no-body-in-particular <73504627+no-body-in-particular@users.noreply.github.com>
2025-04-06 13:29:51 +0200

    Update xgt_bms.ino

--- xgt_bms.ino
+++ xgt_bms.ino
@@ -2,6 +2,8 @@
 #include <XGTBms.h>
 xgt_bms bms(9); // RX, TX
 
+//BMS for xgt batteries, simple protocol implementation
+//not needed for the battery analysis tool, just included for complete documentation of the protocol
 
 void setup() {
 
